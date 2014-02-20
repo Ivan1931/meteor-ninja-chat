7aa461f219408a0b20756a50942d35b0168339e0
jonah-hooper <jonah.graham.hooper@gmail.com>
2014-02-20 21:22:38 +0200

    Added boards feature. Some jquery effects. fixed small bugs

--- chat.js
+++ chat.js
@@ -1,11 +1,62 @@
 Messages = new Meteor.Collection('messages');
+Boards = new Meteor.Collection('boards');
 
 if (Meteor.isClient) {
-  Template.messages.messages = function () {
-    return Messages.find({}, {sort: {time: -1}});
+  fadeEffect = function (instance) {
+      $(instance.firstNode).hide();
+      $(instance.firstNode).fadeIn();
   }
 
-  Template.entryfield.events = {
+  CURRENT_BOARD = function () { return "boardID" }
+
+  boardNameIsValid = function (newBoardName) {
+    return (newBoardName != '') && (Boards.findOne({name: newBoardName}, {}) == undefined)
+  }
+
+  Template.boards.boards = function () {
+    return Boards.find({}, {sort: {name: -1}});
+  }
+
+  Template.messages.messages = function () {
+    return Messages.find({boardID: Session.get('boardID')}, {sort: {name: -1}});
+  }
+
+  Template.currentBoard.currentBoard = function () {
+    return Session.get(CURRENT_BOARD());
+  }
+
+  Template.currentBoard.rendered = function () {
+    fadeEffect(this);
+  }
+
+  Template.messages.rendered = function () {
+    fadeEffect(this);
+  }
+
+  Template.boards.events = {
+    'click .board-name-display-item' : function (event) {
+      var nextName = event.currentTarget.innerHTML;
+      Session.set(CURRENT_BOARD(), nextName);
+    }
+  }
+
+  Template.boardEntry.events = {
+    'keydown #board-name' : function () {
+      if (event.which == 13) {
+        var name = $('#board-name').val();
+        if (boardNameIsValid(name)) {
+          Boards.insert({
+            name: name,
+            time: Date.now(),
+          });
+          Session.set(CURRENT_BOARD(), name);
+        }
+        $('#board-name').val('');
+      }
+    }
+  }
+
+  Template.entryField.events = {
     'keydown #message': function () {
       if (event.which == 13) {
         var name = $('#name').val();
@@ -14,10 +65,10 @@ if (Meteor.isClient) {
           Messages.insert({
             name: name,
             message: message,
-            time: Date.now()
+            time: Date.now(),
+            boardID: Session.get(CURRENT_BOARD())
           });
         }
-        $('#name').val('');
         $('#message').val('');
       }
     }
